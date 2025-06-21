d05c40a419b482d81b60856382c39abd28561902
Guilherme Lourenço <guilhermelourenco106@gmail.com>
2025-06-21 14:40:29 -0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,15 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS
 from classifier import classify_feedback
-from database import save_feedback, get_all_feedbacks, init_db
+from database import save_feedback, get_all_feedbacks
 
-init_db()
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="frontend_files", template_folder="html_views")
+CORS(app)
+
+@app.route("/")
+def home():
+    return render_template("inder.html")
 
 @app.route("/feedback", methods=["POST"])
 def receive_feedback():
@@ -15,10 +20,23 @@ def receive_feedback():
     return jsonify({"category": category}), 200
 
 @app.route("/feedbacks", methods=["GET"])
-def list_feedbacs():
-    feedbacks = get_all_feedbacks()
+def list_feedbacks():
+    category = request.args.get("category")
+    feedbacks = get_all_feedbacks(category)
     return jsonify(feedbacks), 200
 
 if __name__ == "__main__":
+    from database import init_db
+    init_db()
     app.run(debug=True)
 
+
+"""
+caso precise apagar os feedbacks
+
+if __name__ == "__main__":
+    from database import init_db, delete_all_feedbacks
+    init_db()
+    delete_all_feedbacks
+    app.run(debug=True)
+"""
